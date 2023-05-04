978417dce6caf480284e229ab32d15c34b8c4201
Danita Codes <danitacodes@gmail.com>
2023-05-04 08:37:17 -0700

    working on conditional rendering

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ var app = new Vue ({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks."
+        description: "A pair of warm, fuzzy socks.",
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        altText: 'A pair of socks',
+        link: 'https://www.danitacodes.com',
+        inventory: 100,
+        onSale: true
     }
 })

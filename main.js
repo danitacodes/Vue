8de1fbff293dfec2397cf5247d4dd9b2ffcb9e41
Danita Codes <danitacodes@gmail.com>
2023-05-04 07:57:25 -0700

    added stylesheet

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-var product = "Socks"
+// var product = "Socks";
+
+var app = new Vue ({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks."
+    }
+})

e4691c5adb1e5e5389834b1f3d0993225ae04f1b
Danita Codes <danitacodes@gmail.com>
2023-05-08 09:06:56 -0700

    working on list rendering, event handling, class binding

--- main.js
+++ main.js
@@ -8,7 +8,34 @@ var app = new Vue ({
         image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
         altText: 'A pair of socks',
         link: 'https://www.danitacodes.com',
-        inventory: 100,
-        onSale: true
+        inventory: 0,
+        inStok: false,
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+            },
+            {
+                variantId: 2234,
+                variantColor: "blue",
+                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+        sizes: ["small", "medium", "large", "extra-large"],
+        cart: 0
+    },
+    methods: {
+        addToCart: function () {
+            this.cart +=1
+        },
+        updateProduct: function (variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart: function () {
+            this.cart -=1
+        }
     }
 })
